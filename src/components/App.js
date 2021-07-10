278bf1b182f6485dff007543491c1a44be340dcb
sevdas <sshukrieva@gmail.com>
2021-07-10 22:52:23 +0100

    add counter

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,10 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 export default function App() {
   //Keep track of the state of the text
+  //Hold current value of the count down timer and display
   const [text, setText] = useState("");
+  const [timeRemaining, setTimeRemaining] = useState(15);
 
   const handleChange = (event) => {
     const { value } = event.target;
@@ -16,18 +18,24 @@ export default function App() {
     return countTextArr.filter((word) => word !== "").length;
   };
 
+  // Update timer every time timeRemaining changes.
+  useEffect(() => {
+    timeRemaining > 0 &&
+      setTimeout(() => setTimeRemaining((prevTime) => prevTime - 1), 1000);
+  }, [timeRemaining])
+
   return (
     <div>
       <h1>How fast do you type?</h1>
       <textarea type="text" value={text} onChange={handleChange} />
-      <h4>Remaining Time:</h4>
+      <h4>Remaining Time:{timeRemaining}</h4>
       <button
         onClick={() => console.log(calculateWords(text))}
         className="button"
       >
         Start
       </button>
-      <h1>Word Count: </h1>
+      <h1>Word Count:</h1>
     </div>
   );
 }
