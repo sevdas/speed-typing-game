cd79238f05a4f45c34d3f65bd90b1ecac8a26833
sevdas <sshukrieva@gmail.com>
2021-07-11 23:36:02 +0100

    create custom hook and add all the logic into

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 
 Speed typing game that when started automatically receives focus and lets the user start typing right away, then when time runs out the text area becomes disabled giving the number of words the user were able to type in the given time.
 
-This is a client side project using React useState, useEffect, useRef hooks and custom hook created.
+This is a client side project using React useState, useEffect, useRef, and custom hook created.
 
 [Visit:] ()
 
--- src/components/App.js
+++ src/components/App.js
@@ -1,53 +1,16 @@
 import React, { useState, useEffect, useRef } from "react";
+import useWordGame from "../hooks/useWordGame";
 
 export default function App() {
-  const STARTING_TIME = 2;
-  //Keep track of the state of the text
-  //Hold current value of the count down timer and display
-  const [text, setText] = useState("");
-  const [timeRemaining, setTimeRemaining] = useState(STARTING_TIME);
-  const [isTimeRunning, setIsTimeRunning] = useState(false);
-  const [wordCount, setWordCount] = useState(0);
-  const textBoxRef = useRef(null);
-
-  const handleChange = (event) => {
-    const { value } = event.target;
-    setText(value);
-  };
-
-  //Calculate the number of words in the textarea field
-  const calculateWordCount = (countText) => {
-    const countTextArr = text.trim().split(" ");
-    return countTextArr.filter((word) => word !== "").length;
-  };
-
-  //After the game ends, make it so the user can click the start button again to display a second time.
-  //Make the input box focus immediately when the game starts
-  const startGame = () => {
-    setIsTimeRunning(true);
-    setTimeRemaining(STARTING_TIME);
-    setText("");
-    textBoxRef.current.disabled = false;
-    textBoxRef.current.focus();
-  };
-
-  const endGame = () => {
-    setIsTimeRunning(false);
-    setWordCount(calculateWordCount(text));
-  };
-
-  //Update timer every time timeRemaining value changes.
-  //Display number of the words the user typed in once timer reaches 0.
-  useEffect(() => {
-    if (isTimeRunning && timeRemaining > 0) {
-      setTimeout(() => {
-        setTimeRemaining((time) => time - 1);
-      }, 1000);
-    } else if (timeRemaining === 0) {
-      endGame();
-    }
-  }, [timeRemaining, isTimeRunning]);
-
+  const {
+    textBoxRef,
+    text,
+    handleChange,
+    isTimeRunning,
+    timeRemaining,
+    startGame,
+    wordCount,
+  } = useWordGame();
   //Disable the button when time is running
   //Disable the text area when time is not running
   return (
@@ -68,8 +31,3 @@ export default function App() {
     </div>
   );
 }
-
-// User has to click the start button and then to click up into the input box to start typing.
-// Clicking start button clears the box which could be better.
-// Clicking button more then once freaks out the numbers. Every time we click game start, it creates many versions of our useEffect and setTimeout. Setting out state bunch of times which causes bugs. To fix it disable the button when time is running.
-//Focus on text area automatically when user clicks on the start button without them needing to click text area manually so they can start typing immediately.
